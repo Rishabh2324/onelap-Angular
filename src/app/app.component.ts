b021ed164e89ac8b546e8c7f3863de2b254b837c
Rishabh2324 <rishabh2401jain@gmail.com>
2019-10-28 23:26:56 +0530

    deleted some files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sae-website';
-  public awm = "SAE";
-  public message = "";
+  title = 'onelap-Website';
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from  '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
-import { HeroesDetailComponent } from './heroes-detail/heroes-detail.component';
-import { HttpClientModule} from '@angular/common/http';
-import { HeroesService } from './heroes.service';
-import { from } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
+import { MainComponent } from './main/main.component';
+import { HomepageComponent } from './main/homepage/homepage.component';
+import { NavbarComponent } from './main/navbar/navbar.component';
+import { FooterComponent } from './main/footer/footer.component';
+import { HeaderComponent } from './main/homepage/header/header.component';
+import { FeaturesComponent } from './main/homepage/features/features.component';
+import { WorkstepsComponent } from './main/homepage/worksteps/worksteps.component';
+import { ClientStoriesComponent } from './main/homepage/client-stories/client-stories.component';
+import { CallbackFormComponent } from './main/homepage/callback-form/callback-form.component';
+import { TrackItemComponent } from './main/homepage/track-item/track-item.component';
+import { TopHeaderComponent } from './main/top-header/top-header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
-    HeroesDetailComponent,
-    
+    MainComponent,
+    HomepageComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent,
+    FeaturesComponent,
+    WorkstepsComponent,
+    ClientStoriesComponent,
+    CallbackFormComponent,
+    TrackItemComponent,
+    TopHeaderComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -23,7 +39,7 @@ import { from } from 'rxjs';
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [HeroesService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
